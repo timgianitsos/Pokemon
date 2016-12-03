b7b049f63e3e0691179f5bd97e0f0df82935a902
Nik Gianitsos <Gianitsos04@icloud.com>
2016-12-03 11:37:01 -0600

    change trainer battle

--- TrainerBattle.java
+++ TrainerBattle.java
@@ -1,8 +1,8 @@
 class TrainerBattle {
 	public static void main(String[] args) {
-		Pokemon p1 = new Pokemon(PokemonEnum.CHARIZARD);
-		Pokemon p2 = new Pokemon(PokemonEnum.DRAGONITE);
-		p1.useAttack(Attack.DRAGON_CLAW, p2);
+		Pokemon p1 = new Pokemon(PokemonEnum.MEGA_CHARIZARD_Y);
+		Pokemon p2 = new Pokemon(PokemonEnum.VENUSAUR);
+		Pokemon.doTurn(p1, p2);
 	}
 }
 
